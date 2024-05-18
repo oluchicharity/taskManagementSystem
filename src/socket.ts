fa4dec893b8f052d595aa883ae745f92aededa3f
Agbakwuru Oluchi <lulu@Agbakwurus-MacBook-Air.local>
2024-05-18 15:30:05 +0100

    updated

--- src/socket.ts
+++ src/socket.ts
@@ -1,26 +1,3 @@
-// import { Server as SocketIOServer, Socket } from 'socket.io';
-// import http from 'http';
-
-// let io: SocketIOServer | null = null;
-
-// export const initSocketIO = (server: http.Server): void => {
-//   io = new SocketIOServer(server, {
-//     cors: {
-//       origin: '*',
-//     },
-//   });
-
-//   io.on('connection', (socket: Socket) => {
-//     console.log('A client connected');
-
-//     socket.on('disconnect', () => {
-//       console.log('A client disconnected');
-//     });
-//   });
-// };
-
-// export default io;
-
 import { Server as SocketIOServer, Socket } from 'socket.io';
 import http from 'http';
 
